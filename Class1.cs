7fab654cb80dc0806fa5ccaa9604f30bbc913161
Dadasiekgigachad <155850628+Dadasiekgigachad@users.noreply.github.com>
2024-05-11 19:57:55 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using SDG.Unturned;
+using Rocket.Core.Plugins;
+using HarmonyLib;
+
+namespace NoCarBoooM
+{
+    [HarmonyPatch]
+    public class Class1 : RocketPlugin
+    {
+        private static Harmony harmony;
+
+        protected override void Load()
+        {
+            harmony = new Harmony("com.mymod.nocarboooom");
+            harmony.PatchAll();
+
+        }
+
+
+        [HarmonyPatch(typeof(InteractableVehicle), "explode")]
+        [HarmonyPrefix]
+        public static bool Prefix(InteractableVehicle __instance)
+        {
+            return false;
+        }
+    }
+}
